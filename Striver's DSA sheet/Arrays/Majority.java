4bf58bfc4708c2152c5d3577f8487170c5da846c
AnuragM7666 <anuragmankapure1008@gmail.com>
2025-03-27 18:22:58 +0530

    Moore's Voting Algorithm done

---
+++ Striver's DSA sheet/Arrays/Majority.java
@@ -0,0 +1,47 @@
+// import java.util.Arrays;
+public class Majority {
+    public static int MajorityElement(int[] arr) {
+        int n = arr.length;
+
+        int freq = 0;
+        int major = 0;
+//MOORE's algorithm
+        for (int i = 0; i < n; i++) {
+            if (freq == 0) {
+                major = arr[i];
+                freq = 1;
+            }
+            if (arr[i] == major) {
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+        return major;
+
+
+
+        //using sorting technique
+
+        // Arrays.sort(arr);
+        // int frequency = 1;
+        // int major = arr[0];
+        // for (int i = 1; i < n; i++) {
+        //     if (arr[i] == arr[i - 1]) {
+        //         frequency++;
+        //     } else {
+        //         frequency = 1;
+        //         major = arr[i];
+        //     }
+        //     if (frequency > n / 2) {
+        //         return major;
+        //     }
+        // }
+        // return major;
+    }
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 4, 4, 4, 4, 4};
+        int result = MajorityElement(arr);
+        System.out.println(result);
+    }
+}
---
+++ Striver's DSA sheet/Arrays/PairSum.java
@@ -0,0 +1,33 @@
+//retujrn pair of elements whose sum is equal to target in a sorted array
+import java.util.Arrays;
+import java.util.Vector;
+public class PairSum {
+    public static int[] SubSum(int[] arr, int target) {
+        Vector<Integer> Pair = new Vector<Integer>();
+
+        int i = 0;
+        int j = arr.length - 1;
+        int sum = 0;
+        while (i < j) {
+            sum = arr[i] + arr[j];
+            if (sum < target) {
+                i++;
+            } else if (sum > target) {
+                j--;
+            } else {
+                Pair.add(arr[i]);
+                Pair.add(arr[j]);
+                return new int[] { arr[i], arr[j] };
+            }
+        }
+        return new int[] {};
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 6 };
+        int target = 6;
+        int[] result = SubSum(arr, target);
+        System.out.println(Arrays.toString(result));
+    }
+}
