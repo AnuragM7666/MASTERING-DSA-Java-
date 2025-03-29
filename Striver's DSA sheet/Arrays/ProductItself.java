dddcd673784b2ff45c8cf023de4dc9271cb89967
AnuragM7666 <anuragmankapure1008@gmail.com>
2025-03-29 17:36:21 +0530

    done the product of the array except self problem

---
+++ Striver's DSA sheet/Arrays/ProductItself.java
@@ -0,0 +1,46 @@
+public class ProductItself {
+    public static int[] ArrProdExceIt(int[] nums) {
+        //Brute force approach
+
+        int n = nums.length;
+        int[] prod = new int[n];
+        int suffix = 1;
+
+        for(int i = 0; i < n; i++) {
+            prod[i] = 1;
+        }
+        //prefix product
+        for (int i = 1; i < n; i++) {
+            prod[i] = prod[i - 1] * nums[i - 1];
+        }
+        //suffix product
+        for (int i = n - 2; i >= 0; i--) {
+            suffix *= nums[i + 1];
+            prod[i] *= suffix;
+        }
+
+        //Brute force approach
+        
+        // for (int i = 0; i < n; i++) {
+        //     prod[i] = 1;
+        // }
+
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (i != j) {
+        //             prod[i] *= nums[j];
+        //         }
+        //     }
+        // }
+        return prod;
+    }
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5 };
+        int[] result = ArrProdExceIt(nums);
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+        System.out.println();
+    }
+}
