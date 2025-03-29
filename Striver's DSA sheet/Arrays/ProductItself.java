1a0a57b86427f02467342fb2a0aadb4f4af77b3d
AnuragM7666 <anuragmankapure1008@gmail.com>
2025-03-29 17:37:39 +0530

    added Q

--- Striver's DSA sheet/Arrays/ProductItself.java
+++ Striver's DSA sheet/Arrays/ProductItself.java
@@ -1,3 +1,7 @@
+//Given an integer array nums, return an array answer such that answer[i] is equal to 
+//the product of all the elements of nums except nums[i].
+//The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+//You must write an algorithm that runs in O(n) time and without using the division operation.
 public class ProductItself {
     public static int[] ArrProdExceIt(int[] nums) {
         //Brute force approach
@@ -20,7 +24,7 @@ public class ProductItself {
         }
 
         //Brute force approach
-        
+
         // for (int i = 0; i < n; i++) {
         //     prod[i] = 1;
         // }
