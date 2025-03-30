0563e14fb6b89f3fa1b796844076679db62bd506
AnuragM7666 <anuragmankapure1008@gmail.com>
2025-03-30 18:09:09 +0530

    done the mountain array problem

---
+++ Striver's DSA sheet/Binary search algo/MountainArr.java
@@ -0,0 +1,30 @@
+//You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+//Return the index of the peak element.
+//Your task is to solve it in O(log(n)) time complexity.
+
+public class MountainArr {
+    public static int Mountainpeak(int[] arr) {
+        int start = 1;
+        int end = arr.length - 2;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+
+            if (arr[mid - 1] < arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 10, 9, 8, 7, 6, 2, 1 };
+        int peakIndex = Mountainpeak(arr);
+        System.out.println("The peak index is: " + peakIndex);
+    }
+}
