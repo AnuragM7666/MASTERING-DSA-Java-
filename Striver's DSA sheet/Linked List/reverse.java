994b6bf83ce2ff55de5e07c14ce02aa15170f50a
AnuragM7666 <anuragmankapure1008@gmail.com>
2025-05-03 11:01:33 +0530

    done the reverse linked list problem

---
+++ Striver's DSA sheet/Linked List/reverse.java
@@ -0,0 +1,51 @@
+public class reverse {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public  void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + " ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        reverse solution = new reverse();
+
+        System.out.println("Original list:");
+        solution.printList(head);
+
+        ListNode reversed = solution.reverseList(head);
+
+        System.out.println("Reversed list:");
+        solution.printList(reversed);
+    }
+}
