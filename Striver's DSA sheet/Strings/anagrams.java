4f7190b17a1312bae3296fbb9f52c359623ee19f
AnuragM7666 <anuragmankapure1008@gmail.com>
2025-07-08 14:07:40 +0530

    done the anagram problem in Strings

---
+++ Striver's DSA sheet/Strings/anagrams.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+//two strings are anagrams if they contain the same characters in the same frequency
+public class anagrams {
+    public static boolean isAnagram(String s1, String s2) {
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        return Arrays.equals(c1, c2);
+    }
+    public static void main(String[] args) {
+        String s1 = "listen";
+        String s2 = "silent";
+
+        if (isAnagram(s1, s2)) {
+            System.out.println(s1 + " and " + s2 + " are anagrams.");
+        } else {
+            System.out.println(s1 + " and " + s2 + " are not anagrams.");
+        }
+    }
+}
