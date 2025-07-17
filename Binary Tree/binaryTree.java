5dfc78c526627c07252708960319b486be4908d0
AnuragM7666 <anuragmankapure1008@gmail.com>
2025-07-17 14:08:13 +0530

    done the travrsal of a binary tree.

--- Binary Tree/binaryTree.java
+++ Binary Tree/binaryTree.java
@@ -1,3 +1,5 @@
+import java.util.Vector;
+
 public class binaryTree {
     static class Node {
         int data;
@@ -7,5 +9,50 @@ public class binaryTree {
             data = val;
             left = right = null;    
         }
+    }
+    static int index=-1;
+
+    Node buildTree(int[] preorder) {
+        index++;
+        if (preorder[index] == -1) {
+            return null;
+        }
+        Node root = new Node(preorder[index]);
+        root.left = buildTree(preorder);
+        root.right = buildTree(preorder);
+        return root;
+    }
+
+    void preorderTraversal(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+    }
+
+    void inorderTraversal(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left);
+        System.out.print(root.data + " ");
+        inorderTraversal(root.right);
+    }
+    public static void main(String[] args) {
+        binaryTree tree = new binaryTree();
+        int[] preorder = { 1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1 };
+        Node root = tree.buildTree(preorder);
+        System.out.println("Binary tree constructed from preorder traversal.");
+        System.out.println("Root node value: " + root.data);
+        System.out.println("Root node value: " + root.left.data);
+        System.out.println("Root node value: " + root.right.data);
+        System.out.print("Preorder traversal of the binary tree: ");
+        tree.preorderTraversal(root);
+        System.out.println();
+        System.out.print("Inorder traversal of the binary tree: ");
+        tree.inorderTraversal(root);
+        System.out.println();
     }
 }
