0033a9af766d27f0d685d26def14a8c9698a3475
AnuragM7666 <anuragmankapure1008@gmail.com>
2025-07-26 14:01:28 +0530

    done the majority element problem 2 in arrays

---
+++ Striver's DSA sheet/Arrays/majority2.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class majority2 {
+    public List<Integer> major(int[] arr) {
+        int n = arr.length;
+        List<Integer> ans = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (map.get(key) > n / 3) {
+                ans.add(key);
+            }
+        }
+        Collections.sort(ans);
+        return ans;
+    }
+    public static void main(String[] args) {
+        majority2 obj = new majority2();
+        int[] arr = { 1, 2, 3, 4, 4, 2, 2, 4, 4, 2, 2, 4, 4 };
+        List<Integer> result = obj.major(arr);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
